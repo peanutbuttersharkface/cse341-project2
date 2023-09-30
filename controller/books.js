e678b6df94027aaadd789a8e66e8d1d550c7ecb9
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-09-30 17:49:40 -0600

    controller update

--- controller/books.js
+++ controller/books.js
@@ -2,25 +2,42 @@ const mongodb = require('../data/database');
 
 const ObjectId = require('mongodb').ObjectId;
 
-
-const getAllBooks = async (req, res) => {
+const getAllBooks = (req, res) => {
     //#swagger.tags=['Books']
-    const result = await mongodb.getDatabase().db().collection('Books').find();
-    result.toArray().then((Books) => {
+    mongodb
+      .getDatabase()
+      .db()
+      .collection('Books')
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(Books);
-    });
-};
+        res.status(200).json(lists);
+      });
+  };
+  const getSingleBook = (req, res) => {
+     //#swagger.tags=['Books']
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid book id to find a book.');
+    }
+    const bookId = new ObjectId(req.params.id);
+    mongodb
+      .getDatabase()
+      .db()
+      .collection('Books')
+      .find({ _id: bookId })
+      .toArray((err, result) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(result[0]);
+      });
+  };
 
-const getSingleBook = async (req, res) =>{
-    //#swagger.tags=['Books']
-    const BookId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Books').find({_id:bookId});
-    result.toArray().then((Books) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(Books[0]);
-    });
-};
+
 
 const createBook = async(req, res) => {
      //#swagger.tags=['Books']
@@ -36,7 +53,7 @@ const createBook = async(req, res) => {
     };
     const response = await mongodb.getDatabase().db().collection('Books').insertOne(book);
     if (response.acknowledged){
-        res.status(204).send();
+        res.status(201).json(response);
     } else{
         res.status(500).json(response.error || 'Some error occurred while updating the book.');
     }
@@ -44,9 +61,9 @@ const createBook = async(req, res) => {
 
 const updateBook = async(req, res) => {
     //#swagger.tags=['Books']
-    //if (!ObjectId.isValid(req.params.id)) {
-      //  res.status(400).json('Must use a valid contact id to update a contact.');
-    //}else{
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid book id to update a book.');
+    }else{
     const bookId = new ObjectId(req.params.id);
     const book = {
         Title: req.body.Title,
@@ -59,19 +76,24 @@ const updateBook = async(req, res) => {
         Category: req.body.Category
     };
 // collection is the name of the collection that you have the json file in Mongodb
-    const response = await mongodb.getDatabase().db().collection('Books').replaceOne({ _id:bookId }, book);
-    if (response.modifiedCount > 0){
-        res.status(204).send();
-    } else{
-        res.status(500).json(response.error || 'Some error occurred while updating the book.');
+   const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection('Books')
+    .replaceOne({ _id: bookId }, book);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+    res.status(204).send();
+    } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the books.');
     }
-};  
+  };  
 
 const deleteBook = async(req, res)=> {
      //#swagger.tags=['Books']
-   // if (!ObjectId.isValid(req.params.id)) {
-    //    res.status(400).json('Must use a valid contact id to delete a contact.');
-   // }else{
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid book id to delete a book.');
+    }else{
     const bookId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('Books').deleteOne({_id:bookId});
     if(response.deleteCount > 0){
@@ -79,7 +101,7 @@ const deleteBook = async(req, res)=> {
     }else {
         res.status(500).json(response.error || 'Some error ocurred while deleting the book.');
     }
-};
+}}};
 
 
 module.exports = {
--- controller/movies.js
+++ controller/movies.js
@@ -2,28 +2,46 @@ const mongodb = require('../data/database');
 
 const ObjectId = require('mongodb').ObjectId;
 
-const getAllMovies = async (req, res) => {
+const getAllMovies = (req, res) => {
     //#swagger.tags=['Movies']
-    const result = await mongodb.getDatabase().db().collection('Movies').find();
-    result.toArray().then((Movies) => {
+    mongodb
+      .getDatabase()
+      .db()
+      .collection('Movies')
+      .find()
+      .toArray((err, lists) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(Movies);
-    });
-};
-
-const getSingleMovie = async (req, res) =>{
-    //#swagger.tags=['Movies']
+        res.status(200).json(lists);
+      });
+  };
+  const getSingleBook = (req, res) => {
+     //#swagger.tags=['Movies']
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid book id to find a movie.');
+    }
     const movieId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Movies').find({_id:movieId});
-    result.toArray().then((Movies) => {
+    mongodb
+      .getDatabase()
+      .db()
+      .collection('Movies')
+      .find({ _id: movieId })
+      .toArray((err, result) => {
+        if (err) {
+          res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(Movies[0]);
-    });
-};
+        res.status(200).json(result[0]);
+      });
+  };
+
+
 
 const createMovie = async(req, res) => {
      //#swagger.tags=['Movies']
-    const movie = {
+     const movie = {
         Name: req.body.Name,
         YearReleased: req.body.YearReleased,
         DateReceived: req.body.DateReceived,
@@ -35,14 +53,17 @@ const createMovie = async(req, res) => {
     };
     const response = await mongodb.getDatabase().db().collection('Movies').insertOne(movie);
     if (response.acknowledged){
-        res.status(204).send();
+        res.status(201).json(response);
     } else{
         res.status(500).json(response.error || 'Some error occurred while updating the movie.');
     }
 };
 
-const updateMovie = async(req, res) => {
-   //#swagger.tags=['Movies']
+const updateMovies = async(req, res) => {
+    //#swagger.tags=['Movies']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid movie id to update a movie.');
+    }else{
     const movieId = new ObjectId(req.params.id);
     const movie = {
         Name: req.body.Name,
@@ -52,18 +73,27 @@ const updateMovie = async(req, res) => {
         Actor: req.body.Actor,
         Actress: req.body.Actress,
         NumberOfCopies: req.body.NumberOfCopies
+        
     };
 // collection is the name of the collection that you have the json file in Mongodb
-    const response = await mongodb.getDatabase().db().collection('Movies').replaceOne({_id:movieId},movie);
-    if (response.modifiedCount > 0){
-        res.status(204).send();
-    } else{
-        res.status(500).json(response.error || 'Some error occurred while updating the movie.');
+   const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection('Movies')
+    .replaceOne({ _id: movieId }, movie);
+    console.log(response);
+    if (response.modifiedCount > 0) {
+    res.status(204).send();
+    } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the movie.');
     }
-};  
+  };  
 
 const deleteMovie = async(req, res)=> {
-     //#swagger.tags=['Movies']
+     //#swagger.tags=['Movie']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use a valid movie id to delete a movie.');
+    }else{
     const movieId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('Movies').deleteOne({_id:movieId});
     if(response.deleteCount > 0){
@@ -71,7 +101,8 @@ const deleteMovie = async(req, res)=> {
     }else {
         res.status(500).json(response.error || 'Some error ocurred while deleting the movie.');
     }
-};
+}}};
+
 
 module.exports = {
     getAllMovies,
