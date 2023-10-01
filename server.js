f5cc214022c3bcf400e7ab8ef9f44101359ee770
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-09-30 22:27:17 -0600

    app

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app
       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       next();
 });
-  app.use('/', require('./routes'));
+app.use('/', require('./routes'));
 
 process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `Caught exception: ${err}\n`, `Exception origin: ${origin}`);
