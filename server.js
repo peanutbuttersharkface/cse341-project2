c3ea07166d16c3c2f05a7116b07c88a82679a22a
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-09-30 22:19:59 -0600

    order

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-const port = process.env.PORT || 3000;
 const app = express();
 
+const port = process.env.PORT || 3000;
 
 
 
