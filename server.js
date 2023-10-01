4a174f5a2b75ddbcba635281947b8b57bf0c0162
peanutbuttersharkface <peanutbuttersharkface@gmail.com>
2023-09-30 20:07:22 -0600

    app.use

--- server.js
+++ server.js
@@ -11,9 +11,14 @@ app
    .use(bodyParser.json())
    .use((req, res, next) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader(
+         'Access-Control-Allow-Headers',
+         'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+      );
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
       next();
-})
-   .use('/', require('./routes'));
+});
+  app.use('/', require('./routes'));
 
 process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `Caught exception: ${err}\n`, `Exception origin: ${origin}`);
